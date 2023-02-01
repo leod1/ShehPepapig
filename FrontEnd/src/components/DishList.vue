9137722025fd00355fed32fcd3ec4d207f2039cb
StevenIsdr <laglobule59260@gmail.com>
2023-02-01 11:25:37 +0100

    Inital

---
+++ FrontEnd/src/components/DishList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="dish in dishes" :key="dish.id">
+
+
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "DishList",
+  data() {
+    dishes: []
+  }
+  
+    created() {
+    await fetch('http://localhost:3000/dishes')
+      .then(response => response.json())
+      .then(dishes => {
+        this.dishes = dishes;
+      });
+  }
+
+}
+</script>
