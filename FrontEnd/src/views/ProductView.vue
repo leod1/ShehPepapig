6a8ea4df3473596b07648024bddfc55667f7318b
StevenIsdr <laglobule59260@gmail.com>
2023-02-10 09:48:33 +0100

    feu$

---
+++ FrontEnd/src/views/ProductView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+export default {
+  name: "LogView",
+  beforeMount() {
+    this.getProduct();
+  },
+  data() {
+    return {
+      product: Object,
+      id: this.$route.params.id,
+    }
+  },
+  methods: {
+    getProduct: async function () {
+      await fetch("/product/" + this.id, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json()).then((res) => {
+        if (res.message === "ok") {
+          this.product = res.product;
+        } else {
+          this.displayError = true;
+        }
+      });
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
