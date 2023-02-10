3d28e6fc314fc2cae387e3fa7c0344f88b850df2
thomas cls <deanint59@gmail.com>
2023-02-10 10:54:46 +0100

    cors

--- back/src/main/java/com/example/product/ws/CategoryWs.java
+++ back/src/main/java/com/example/product/ws/CategoryWs.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(value = ApiRegistration.CATEGORY_WS)
 public class CategoryWs {
 
