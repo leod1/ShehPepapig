7b6c622d1e3ef3c4937b38565ee257b7d4986e56
thomas cls <deanint59@gmail.com>
2023-02-10 09:20:22 +0100

    back

---
+++ back/src/main/java/com/example/product/pojo/Product.java
@@ -0,0 +1,57 @@
+package com.example.product.pojo;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String image;
+
+    public Product() {
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Product(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ back/src/main/java/com/example/product/ws/ApiRegistration.java
@@ -0,0 +1,6 @@
+package com.example.product.ws;
+
+public interface ApiRegistration {
+
+    public String PRODUCT_WS = "/product";
+}
